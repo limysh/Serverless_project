26f9f05b0e76c800992451ecabb9c17c8f6cea40
limyshkalapala <limysh@dal.ca>
2023-06-30 22:06:41 -0300

    game lobby basic structure

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,11 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+<<<<<<< HEAD
 import { getFirestore } from "firebase/firestore";
+=======
+import { getFirestore } from "firebase/firestore"; 
+>>>>>>> 2ae4fb7 (game lobby basic structure)
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,4 +28,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const firestore = getFirestore(app); // Initialize Firestore instance
+<<<<<<< HEAD
 export default app;
+=======
+export default app;
+>>>>>>> 2ae4fb7 (game lobby basic structure)
