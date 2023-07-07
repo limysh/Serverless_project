77efb8042cff4b6c5436cc22985205adfdb38555
limyshkalapala <limysh@dal.ca>
2023-07-07 20:33:21 -0300

    firestore collections implemented, filtering working

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Logout from "./pages/logout";
-import {AuthContextProvider} from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import Dashboard from "./pages/dashboard";
+import GameLobby from "./pages/game"; // Import the GameLobby component
 
 function App() {
-  return <div>
-    <AuthContextProvider>
-      <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<Login />} path="/login" />
-        <Route element={<Logout />} path="/logout" />
-        <Route element={<Dashboard />} path="/dashboard" />
-      </Routes>
-    </AuthContextProvider>
-  </div>;
+  return (
+    <div>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/game-lobby" element={<GameLobby />} /> {/* Add the route for the GameLobby component */}
+        </Routes>
+      </AuthContextProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/game.js
+++ src/pages/game.js
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import { firestore } from "../firebase";
+import { collection, getDocs, doc, setDoc } from "firebase/firestore";
 
 const Game = () => {
   const { logOut, user } = UserAuth();
   const navigate = useNavigate();
-  const [gameLobby, setGameLobby] = useState(null);
+  const [gameLobbies, setGameLobbies] = useState([]);
+  const [filteredLobbies, setFilteredLobbies] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [difficultyFilter, setDifficultyFilter] = useState("");
+  const [timeFrameFilter, setTimeFrameFilter] = useState("");
 
   const handleLogOut = async () => {
     try {
@@ -18,50 +23,139 @@ const Game = () => {
   };
 
   useEffect(() => {
-    // Fetch game lobby data from Firestore or DynamoDB
-    const fetchGameLobby = async () => {
+    // Fetch all game lobbies from Firestore
+    const fetchGameLobbies = async () => {
       try {
-        // Assuming you have a collection named "gameLobbies" in Firestore
-        const gameLobbyRef = firestore.collection("gameLobbies").doc("your-game-lobby-id");
-        const snapshot = await gameLobbyRef.get();
-        if (snapshot.exists()) {
-          const lobbyData = snapshot.data();
-          setGameLobby(lobbyData);
-        } else {
-          // Game lobby not found
-        }
+        const gameLobbiesRef = collection(firestore, "gameLobbies");
+        const snapshot = await getDocs(gameLobbiesRef);
+        const lobbiesData = snapshot.docs.map((doc) => doc.data());
+        setGameLobbies(lobbiesData);
       } catch (err) {
         console.log(err);
       }
     };
 
-    fetchGameLobby();
+    fetchGameLobbies();
   }, []);
+
+  const createGameLobby = async () => {
+    try {
+      const gameLobbiesRef = collection(firestore, "gameLobbies");
+      const gameLobbyId = doc(gameLobbiesRef);
+      const players = [
+        { id: "player1-id", name: "Player 1" },
+        { id: "player2-id", name: "Player 2" },
+        // Add more player objects as needed
+      ];
+
+      const newGameLobby = {
+        id: gameLobbyId.id,
+        name: "My Game Lobby",
+        category: "Trivia",
+        difficulty: "Easy",
+        timeFrame: "30 minutes",
+        players: players,
+        // Add other required fields for the game lobby
+      };
+
+      await setDoc(gameLobbyId, newGameLobby);
+
+      setGameLobbies((prevGameLobbies) => [...prevGameLobbies, newGameLobby]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleJoinGame = (gameId) => {
+    navigate(`/game/${gameId}`);
+  };
+
+  const handleFilterGames = () => {
+    // Filter the game lobbies based on the selected criteria
+    const filteredLobbies = gameLobbies.filter((lobby) => {
+      return (
+        (categoryFilter === "" || lobby.category === categoryFilter) &&
+        (difficultyFilter === "" || lobby.difficulty === difficultyFilter) &&
+        (timeFrameFilter === "" || lobby.timeFrame === timeFrameFilter)
+      );
+    });
+
+    setFilteredLobbies(filteredLobbies);
+  };
+
+  const resetFilter = () => {
+    setCategoryFilter("");
+    setDifficultyFilter("");
+    setTimeFrameFilter("");
+    setFilteredLobbies([]);
+  };
 
   return (
     <div>
       <h1>Game Lobby</h1>
-      {gameLobby ? (
-        <>
-          <h2>Lobby Details</h2>
-          <p>Lobby ID: {gameLobby.id}</p>
-          <p>Game Name: {gameLobby.name}</p>
-          {/* Display other game lobby information */}
+      {/* Display game lobbies */}
+      <h2>Available Game Lobbies</h2>
+      <ul>
+        {(filteredLobbies.length > 0 ? filteredLobbies : gameLobbies).map((lobby) => (
+          <li key={lobby.id}>
+            <h3>{lobby.name}</h3>
+            <p>Category: {lobby.category}</p>
+            <p>Difficulty: {lobby.difficulty}</p>
+            <p>Time Frame: {lobby.timeFrame}</p>
+            {/* Add other lobby details */}
+            <button onClick={() => handleJoinGame(lobby.id)}>Join Lobby</button>
+          </li>
+        ))}
+      </ul>
 
-          {/* Render list of players */}
-          <h2>Players</h2>
-          <ul>
-            {gameLobby.players.map((player) => (
-              <li key={player.id}>{player.name}</li>
-            ))}
-          </ul>
+      {/* Filter game lobbies */}
+      <h2>Filter Game Lobbies</h2>
+      {/* Category filter */}
+      <div>
+        <label>Category:</label>
+        <select
+          value={categoryFilter}
+          onChange={(e) => setCategoryFilter(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="Trivia">Trivia</option>
+          <option value="Puzzle">Puzzle</option>
+          {/* Add more category options as needed */}
+        </select>
+      </div>
+      {/* Difficulty filter */}
+      <div>
+        <label>Difficulty:</label>
+        <select
+          value={difficultyFilter}
+          onChange={(e) => setDifficultyFilter(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="Easy">Easy</option>
+          <option value="Medium">Medium</option>
+          <option value="Hard">Hard</option>
+          {/* Add more difficulty options as needed */}
+        </select>
+      </div>
+      {/* Time Frame filter */}
+      <div>
+        <label>Time Frame:</label>
+        <select
+          value={timeFrameFilter}
+          onChange={(e) => setTimeFrameFilter(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="15 minutes">15 minutes</option>
+          <option value="30 minutes">30 minutes</option>
+          <option value="1 hour">1 hour</option>
+          {/* Add more time frame options as needed */}
+        </select>
+      </div>
+      <button onClick={handleFilterGames}>Filter Lobbies</button>
+      <button onClick={resetFilter}>Reset Filter</button>
 
-          {/* Start game button */}
-          <button onClick={() => console.log("Start game")}>Start Game</button>
-        </>
-      ) : (
-        <p>Loading game lobby...</p>
-      )}
+      {/* Create game lobby button */}
+      <button onClick={createGameLobby}>Create Game Lobby</button>
 
       <button onClick={handleLogOut}>Logout</button>
     </div>
