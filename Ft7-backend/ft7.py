1428b749e40c020e06a7372b10754ad7871a60c1
Kishoreganesh Sundararajan <kishore7403@gmail.com>
2023-07-10 15:08:44 -0300

    edit questions backend updated

--- Ft7-backend/ft7.py
+++ Ft7-backend/ft7.py
@@ -21,34 +21,34 @@ db = firestore.client()
 
 questions = []
 
-# @app.route('/addquestion', methods=['POST'])
-# def add_question():
-#     question_data = request.json
+@app.route('/addquestion', methods=['POST'])
+def add_question():
+    question_data = request.json
 
-#     # Extract the required fields from the question data
-#     question_number = question_data.get('questionNumber')
-#     question_text = question_data.get('questionText')
-#     options = question_data.get('options')
-#     correct_answer_index = question_data.get('correctAnswerIndex')
-#     category = question_data.get('category')
-#     difficulty_level = question_data.get('difficultyLevel')
+    # Extract the required fields from the question data
+    question_number = question_data.get('questionNumber')
+    question_text = question_data.get('questionText')
+    options = question_data.get('options')
+    correct_answer_index = question_data.get('correctAnswerIndex')
+    category = question_data.get('category')
+    difficulty_level = question_data.get('difficultyLevel')
 
-#     doc_ref = db.collection('questions').document('q'+str(question_number))
-#     doc_ref.set({
-#         'questionNumber': question_number,
-#         'questionText': question_text,
-#         'options': options,
-#         'correctAnswerIndex': correct_answer_index,
-#         'category': category,
-#         'difficultyLevel': difficulty_level
-#     })
+    doc_ref = db.collection('questions').document('q'+str(question_number))
+    doc_ref.set({
+        'questionNumber': question_number,
+        'questionText': question_text,
+        'options': options,
+        'correctAnswerIndex': correct_answer_index,
+        'category': category,
+        'difficultyLevel': difficulty_level
+    })
 
 
     
-#     # # Add the question to the questions list
-#     # questions.append(question)
+    # # Add the question to the questions list
+    # questions.append(question)
 
-#     return jsonify({'message': 'Question added successfully'})
+    return jsonify({'message': 'Question added successfully'})
 
 
 @app.route('/editquestion', methods=['POST'])
@@ -65,7 +65,14 @@ def edit_question():
     except Exception as e:
         return jsonify({'message': str(e)}), 500
 
-
+@app.route('/deletequestion', methods=['POST'])
+def delete_document():
+    try:
+        document_id = request.json['document_id']
+        db.collection('questions').document(document_id).delete()
+        return jsonify({'success': True}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
