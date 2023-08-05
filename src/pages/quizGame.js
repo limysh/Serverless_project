10892ecf24c30bb8e05edbfbae7a0f653eca2b64
Kishoreganesh Sundararajan <kishore7403@gmail.com>
2023-08-05 18:21:34 -0300

    time changed to 7 secs

--- src/pages/quizGame.js
+++ src/pages/quizGame.js
@@ -151,8 +151,8 @@ const QuizGame = () => {
   useEffect(() => {
     const questionInterval = setInterval(() => {
       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-      setCountdown(10); // 3 seconds
-    }, 10000); // 3 seconds
+      setCountdown(70); // 3 seconds
+    }, 7000); // 3 seconds
 
     setTimer(questionInterval);
 
@@ -171,7 +171,7 @@ const QuizGame = () => {
 
     // Reset the countdown for the current question
     if (currentQuestionIndex >= 0 && currentQuestionIndex < questionNumbers.length) {
-      setCountdown(10); // 3 seconds
+      setCountdown(70); // 3 seconds
     }
   }, [timer, countdown, currentQuestionIndex, questionNumbers.length]);
 
